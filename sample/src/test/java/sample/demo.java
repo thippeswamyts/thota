3359aeb460088f59891d1c1bf8faaf808bf23a05
thippeswamyts <Thippeswamyhlk98@gmail.com>
2024-04-19 17:12:12 +0530

    first commit

---
+++ sample/src/test/java/sample/demo.java
@@ -0,0 +1,13 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class demo {
+
+	@Test
+	public void sample()
+	{
+		System.out.println("hello for jenkins please execute");
+	}
+	
+}
